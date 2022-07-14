db8e96dc99d4ff2b01cb01d082f5d6e80313097f
verumlotus <universumtremendum@gmail.com>
2022-07-14 14:20:27 -0400

    Add colors

--- components/CustomizedVennDiagram.tsx
+++ components/CustomizedVennDiagram.tsx
@@ -1,4 +1,5 @@
 import { VennDiagram, asSets } from '@upsetjs/react'
+import { useState, useMemo } from 'react'
 
 type SetData = {
     name: string,
@@ -7,13 +8,20 @@ type SetData = {
 
 export default function CustomizedVennDiagram(props: any) {
     const vennDiagramSetData: SetData[] = props.vennDiagramSetData
+    const [selection, setSelection] = useState(null);
+
+    const dataToUse = useMemo(() => {
+        const colors = ['#f47560', '#61cdbb', '#75975e', '#f1e15b', '#e8a838', '#97e3d5'];
+        return asSets(vennDiagramSetData).map((s, i) => ({ ...s, color: colors[i % colors.length] }));
+    }, [vennDiagramSetData])
+
     return (
         <VennDiagram
-          sets={asSets(vennDiagramSetData)}
+          sets={dataToUse}
           width={780}
           height={400}
         //   selection={selection}
-        //   onHover={setSelection}
+        //   onHover={setSelection as any}
         />
     );
 }
