cd884bc37a2a403461ac6857fbc75a224023dbfe
verumlotus <universumtremendum@gmail.com>
2022-07-14 16:09:31 -0400

    react-ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'UA-234560740-2'
+ReactGA.initialize(TRACKING_ID);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
